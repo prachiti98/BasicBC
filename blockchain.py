538a3923a47e80a02a907788eff53cb5d1800337
prachiti98 <prachitiparkar98@gmail.com>
2019-04-17 09:42:05 +0530

    First Commit

---
+++ blockchain.py
@@ -0,0 +1,46 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        #Creating the genesis block
+        self.new_block(previous_hash=1,proof=100)
+        
+    def new_block(self,proof,previous_hash):
+        # Creates a new Block and adds it to the chain
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions ,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]);
+        }
+
+        self.current_transactions=[]
+
+        self.chain.append(block)
+        return block
+    
+    def new_transaction(self,sender,recipient,amount):
+        # Adds a new transaction to the list of transactions
+        self.current_transactions.append({
+            'sender':sender,
+            'recipient':recipient,
+            'amount':amount,
+            })
+        return self.last_block['index'] + 1
+    
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        block_string = json.dumps(block,sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+    
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        return self.chain[-1];
